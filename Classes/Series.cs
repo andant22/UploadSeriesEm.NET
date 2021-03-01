c1613aecc2e749ab0a092d75066785cb068fa3d6
Anderson Antonio <andant22@gmail.com>
2021-03-01 19:25:01 -0300

    Cadastro de Séries em .NET

---
+++ Classes/Series.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Diagnostics;
+
+namespace Series
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero  Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        //Métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+            
+        }
+        public override string ToString()
+        {
+
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine; 
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine; 
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine; 
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+
+            return retorno;
+            
+        }
+
+        public string retornaTitulo() 
+        {
+            return this.Titulo;
+        }
+        public int retornaId()
+        {
+            return this.id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+        
+}
